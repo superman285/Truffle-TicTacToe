e5310a28405e5b38b286bf52cd31a1ece1ab13c4
superman285 <superman285@126.com>
2019-03-30 11:59:58 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
   publicPath: "./",
   devServer: {
     port: 3000,
-    compress: true // 服务器返回浏览器的时候是否启动gzip压缩
+    compress: true
   },
   productionSourceMap: false,
   configureWebpack: {
